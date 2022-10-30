d6bd698af244ae9b906e5d7b3674e64c3c4bfcf0
Jaden Balogh <jadenbalogh@gmail.com>
2022-10-30 16:56:31 -0400

    Add font and stuff

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class Monster : Actor
 {
+    [SerializeField] private float followDelay = 0.2f;
+    [SerializeField] private float leaveDelay = 0.6f;
+
+    private Vector3 targetDir;
+    private Coroutine leaveCR;
+
     protected void Update()
     {
         Player player = GameManager.Player;
-        Vector3 targetDir;
 
         if (!player.IsHidden)
         {
+            if (leaveCR != null)
+            {
+                StopCoroutine(leaveCR);
+                leaveCR = null;
+                Debug.Log("Stopping!!");
+            }
             targetDir = player.transform.position - transform.position;
         }
         else
         {
-            targetDir = transform.position - player.transform.position;
+            if (leaveCR == null) leaveCR = StartCoroutine(LeaveCR());
         }
 
         rigidbody2D.velocity = targetDir.normalized * moveSpeed;
 
         UpdateAnims(true);
+    }
+
+    private IEnumerator LeaveCR()
+    {
+        Debug.Log("Starting!!");
+
+        Player player = GameManager.Player;
+
+        yield return new WaitForSeconds(followDelay);
+
+        targetDir = Vector3.zero;
+
+        yield return new WaitForSeconds(leaveDelay);
+
+        targetDir = transform.position - player.transform.position;
+
+        while (player.IsHidden)
+        {
+            yield return null;
+        }
     }
 }
